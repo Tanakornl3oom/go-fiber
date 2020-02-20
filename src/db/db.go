9afcf828078cfb9445e0c471c8de84e470b9d047
tanakorn <tanakorn@central.tech>
2020-02-20 15:20:51 +0700

    get , get by id , add , edit user

--- src/db/db.go
+++ src/db/db.go
@@ -3,24 +3,163 @@ package db
 import (	"database/sql"
 			_ "github.com/go-sql-driver/mysql"
 			"fmt"
+			"encoding/json"
+			"strconv"
 		)
 // struct name should starts with Capital letter to export
 type Usersdata struct {
-	ID              int
-	Fristname       string
+	ID              int   	`json:"id"`
+	Firstname       string	`json:"firstName"`
+	Lastname        string	`json:"lastName"`
+	Age             int		`json:"age"`
+}
+
+type User struct {
+	Firstname       string
+	Lastname        string
+	Age             string
+}
+
+type UserDB struct {
+	Firstname       string
 	Lastname        string
 	Age             int
 }
 
-func ConnectDB() bool{
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testsck")
-	defer db.Close()
+type TableInDB struct {
+	TablesInDatabase     string
+}
+
+type Response struct {
+	Status       bool
+	Response     interface{}
+}
+
+func ConnectDB() *sql.DB{
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/database")
+	result, _ := db.Query("SHOW TABLES LIKE '%USER%'")
+	for result.Next() {
+		var table TableInDB
+		err := result.Scan(&table.TablesInDatabase)
+		if err != nil {
+			fmt.Println(err)
+		}
+		// check table is exist
+		if table == (TableInDB{}){
+			fmt.Println(table)
+			_, err =  db.Exec("CREATE TABLE USER (ID INT AUTO_INCREMENT,first_name VARCHAR (50) NOT NULL,last_name VARCHAR (50) NOT NULL,age INT NOT NULL,PRIMARY KEY (ID))")
+			if err != nil {
+				fmt.Println("Can't create table user")
+				fmt.Println(err)
+			}
+		}
+	}
+	
 	if err != nil {
 		fmt.Println("connect fail")
-		return false
+		fmt.Println(err)
 	} else {
 		fmt.Println("connect success")
-		return true
+	}
+	return db
+}
+
+func GetUsers(db *sql.DB) []interface{} {
+	result, _ := db.Query("SELECT * FROM USER")
+	var userDataList []interface{}
+	if result == nil {
+		return userDataList
+	}
+	for result.Next() {
+		var user Usersdata
+		err := result.Scan(
+			&user.ID,
+			&user.Firstname,
+			&user.Lastname,
+			&user.Age,
+		)
+
+		if err != nil {
+			panic(err.Error())
+		}
+		userDataList = append(userDataList, PrettyPrint(user))
+	}
+	return userDataList
+}
+
+func GetUserById(db *sql.DB,id int) Usersdata {
+	result, _ := db.Query("SELECT * FROM USER WHERE id = "+strconv.Itoa(id))
+
+	var userById Usersdata
+	if result == nil {
+		return Usersdata{}
+	}
+	for result.Next() {
+	err := result.Scan(
+		&userById.ID,
+		&userById.Firstname,
+		&userById.Lastname,
+		&userById.Age,
+	)
+
+	if err != nil {
+		panic(err.Error())
 	}
 }
 
+	return userById
+}
+
+func AddUser(db *sql.DB,user UserDB) bool {
+	statement, err := db.Prepare("INSERT INTO USER ( first_name, last_name, age) VALUES (?,?,?)")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	_, err = statement.Exec(user.Firstname,user.Lastname,user.Age)
+
+	if err != nil {
+		panic(err.Error())
+		return false
+	}
+	return true
+}
+
+func UpdateUser(db *sql.DB,user Usersdata) Response {
+	statement, err := db.Prepare("UPDATE USER SET first_name= ?,last_name= ? ,age=? WHERE id = ?")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	_, err = statement.Exec(user.Firstname,user.Lastname,user.Age,user.ID)
+
+	var response Response
+	if err != nil {
+		panic(err.Error())
+		response.Status = false
+		response.Response = "Can't update user"
+		return response
+	}else {
+		response.Status = true
+		response.Response = PrettyPrint(user)
+		return response
+	}
+}
+
+func DeleteUser(db *sql.DB,user Usersdata) bool {
+	// statement, _ := db.Prepare(`INSERT INTO user ( firstname, lastname, age
+	// 	)VALUES (?,?,?)`)
+
+	// _, err := statement.Exec(user.Firstname,user.Lastname,user.Age)
+
+	// if err != nil {
+	// 	panic(err.Error())
+	// 	return false
+	// }
+	return true
+}
+
+func PrettyPrint(i interface{}) string {
+    s, _ := json.MarshalIndent(i, "", "\t")
+    return string(s)
+}
--- src/server.go
+++ src/server.go
@@ -2,6 +2,8 @@ package main
 
 import ("github.com/gofiber/fiber"
         "go-fiber/src/db"
+        "strconv"
+        "encoding/json"
         )
 
 
@@ -9,12 +11,115 @@ func main() {
   // Create new Fiber instance:
   app := fiber.New()
 
-  db.ConnectDB()
+  dbConn := db.ConnectDB()
   // Create route on root path, "/":
   app.Get("/", func(c *fiber.Ctx) {
+
     c.Send("Hello, World!")
     // => "Hello, World!"
   })
+
+  app.Get("/users",func(c *fiber.Ctx) {
+
+    var userList = db.GetUsers(dbConn)
+    c.Send(userList)
+  })
+
+  app.Get("/user/:id",func(c *fiber.Ctx) {
+
+    if c.Params("id") == ""  {
+      c.Send("required id")
+    }else{
+      id, e := strconv.Atoi(c.Params("id"))
+      if e != nil {
+        c.Send("id should be number")
+      }else{
+        var user = db.GetUserById(dbConn,id)
+        if user.ID == 0 {
+          c.Send("user id : "+strconv.Itoa(id)+" not found ")
+        }else{
+          c.Send(db.PrettyPrint(user))
+        }
+      }
+    }
+  })
+
+  app.Post("/user",func(c *fiber.Ctx) {
+
+    var body db.User
+    json.Unmarshal([]byte(c.Body()), &body)
+    if body.Firstname == "" || body.Lastname == "" || body.Age == "" {
+      c.Send("required firstName , lastName and age")
+    } else{ 
+      age, e := strconv.Atoi(body.Age)
+      if e != nil {
+        c.Send("age should be number")
+      }else{
+
+        var user = db.UserDB{
+          Firstname : body.Firstname,
+          Lastname :body.Lastname,
+          Age : age}
+    
+        var status = db.AddUser(dbConn,user)
+        if status {
+          c.Send("Success add user : "+db.PrettyPrint(user))
+        }else {
+          c.Send("Fail add user")
+        }
+      }
+    }
+  
+  })
+
+
+  app.Put("/user/:id",func(c *fiber.Ctx) {
+
+    var body db.User
+    json.Unmarshal([]byte(c.Body()), &body)
+
+    if c.Params("id") == ""  {  // require id 
+      c.Send("required id")
+    }else if body.Firstname == "" || body.Lastname == "" || body.Age == "" { // require use body
+      c.Send("required firstName , lastName and age")
+    }else{
+      // check age integer
+      age, e := strconv.Atoi(body.Age)
+      if e != nil {
+        c.Send("age should be number")
+      }else{
+        //check id integer
+        id, e := strconv.Atoi(c.Params("id"))
+        if e != nil {
+          c.Send("id should be number")
+        }else{
+          //check user exist
+          var user = db.GetUserById(dbConn,id)
+          if user.ID == 0 {
+            c.Send("user id : "+strconv.Itoa(id)+" not found ")
+          }else{
+              //edit user
+              user.Firstname =  body.Firstname
+              user.Lastname =  body.Lastname
+              user.Age = age
+
+              //func edit user
+              var editUserResponse db.Response 
+              editUserResponse = db.UpdateUser(dbConn,user)
+              c.Send(editUserResponse.Response)
+            }
+          }
+      }
+    }
+  })
+
+
+
+  app.Delete("/user",func(c *fiber.Ctx) {
+
+    c.Send(true)
+  })
+
 
   app.Get("/value/:value?", func(c *fiber.Ctx) {
     if c.Params("value") != "" {
